a009ea2484eaa61c08bac98f3ccfc62c4554f2e0
AnggaJaunk98 <65926127+Jaunk98@users.noreply.github.com>
2020-06-30 03:19:53 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import time
+
+
+os.system('clear')
+time.sleep(1)
+os.system('figlet hack fb')
+print ("========================================")
+print ("Author   : Jaunk98")
+print ("Youtube  : Termux-Pemula")
+print ("Facebook : Btnzrg")
+print ("========================================")
+print
+time.sleep(1)
+print ("        [+]Pilih Menu[+]        ")
+print
+print ("[1] Tools terkey")
+print ("[2] Tools Sms")
+print ("[3] Tools Call")
+print ("[4] Install bahan")
+pilih = raw_input('[?] pilih :')
+if pilih == "1":
+        os.system('git clone https://github.com/B4N954N2-ID/ter>
+        print ('selanjutnya ketik $cd termux-terkey')
+        print ('lalu ketik python2 run.py')
+elif pilih == "2":
+        os.system('git clone https://github.com/KANG-NEWBIE/Spa>
+        print ('selanjutnya ketik $cd spamsms')
+        print ('lalu ketik python2 main.py')
+elif pilih == "3":
+        os.system("git clone https://github.com/Fukur0-3XP/Spam>
+        print ("selanjutnya ketik $cd SpamCall")
+        print ("lalu ketik $python2 Call.py")
+elif pilih == "4":
+        os.system('pkg update && pkg upgrade')
+        os.system('pkg install pythob2')
+        os.system('pkg install git')
+        os.system('pkg install mechanize')
+        os.system('pkg install requests')
+        os.system('pip install requests mechanize bs4')
+        print ("[+] Suskses Penginstalan")
